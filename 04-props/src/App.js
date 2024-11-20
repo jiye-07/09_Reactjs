67484aa4b0058f3ce80306440fcb4158dca46e70
jiye <cg2522@naver.com>
2024-11-20 16:36:44 +0900

    PROPS: 기본 작업

---
+++ 04-props/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Link, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>리액트 시작</h1>
+    </div>
+  );
+};
+
+export default App;
